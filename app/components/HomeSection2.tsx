c21b41b6569aa036c7372ad72fd9f26141d2f3b2
AroraKaran19 <karora0319@gmail.com>
2024-07-20 02:41:22 +0530

    fixed!!!!!!!!!!

--- app/components/HomeSection2.tsx
+++ app/components/HomeSection2.tsx
@@ -34,7 +34,7 @@ const HomeSection2 = () => {
     <>
       <section id='what-we-provide' className='home-second-section flex flex-col justify-center items-center w-full'>
         <h1 className='section-heading'>What We Provide</h1>
-        <p className='section-sub-heading w-[35%] sm:w-full text-center'>We provide the best possible solution for your business. Here's what we provide:</p>
+        <p className='section-sub-heading w-[35%] sm:w-full text-center'>We provide the best possible solution for your business. Here&apos;s what we provide:</p>
         <div className="home-second-section-content h-full w-full flex flex-col relative">
           <Carousel items={services} />
         </div>
